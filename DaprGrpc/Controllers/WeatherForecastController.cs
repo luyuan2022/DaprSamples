bf320e6878a70b0a48050a9f16ee5e9dfacd10f4
luyuan2022 <94880040+luyuan2022@users.noreply.github.com>
2022-01-18 09:44:26 +0800

    Update WeatherForecastController.cs

--- DaprGrpc/Controllers/WeatherForecastController.cs
+++ DaprGrpc/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DaprGrpc.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warmok", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warmok123", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
